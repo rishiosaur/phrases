66b3ac0e6475cc8d8315519792a026692379523d
Rishi Kothari <hey@rishi.cx>
2024-02-05 11:19:47 -0500

    move api

--- src/pages/api/notion.ts
+++ src/pages/api/notion.ts
@@ -1,13 +1,20 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 
+import { Client } from "@notionhq/client";
+
 type Data = {
   name: string;
 };
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>,
+  res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: "John Doe" });
+  const notion = new Client({
+    auth: process.env.NOTION_TOKEN,
+  });
+
+  const res = await notion.
+  // res.status(200).json({ name: "John Doe" });
 }
